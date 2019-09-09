81cc935e11b16bceb37cb9b535bdda54db311612
grey_hatt <ervinbenez@gmail.com>
2019-09-09 14:05:04 +0800

    redirect to tasks

--- resources/views/vendor/sbadmin2/master.blade.php
+++ resources/views/vendor/sbadmin2/master.blade.php
@@ -53,9 +53,9 @@
           <a class="btn btn-primary" href="{{ route('logout') }}" onclick="event.preventDefault();
                  document.getElementById('logout-form').submit();">Logout</a>
 
-<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-    @csrf
-</form>
+          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+            @csrf
+          </form>
         </div>
       </div>
     </div>
