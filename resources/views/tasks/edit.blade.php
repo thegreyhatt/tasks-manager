2fa1177ef6c48ca51ced6bb68cbce1d7bbbe8cdd
ervinbenez <ervinbenez@gmail.com>
2019-09-23 17:16:51 +0800

    Changed create and update view

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -1,17 +1,17 @@
-@extends('layouts.app')
+@extends('sbadmin2::page')
 
 @section('content')
-    <div class="container">
+    {{-- <div class="container"> --}}
         <div class="row">
-            @include('admin.sidebar')
+            {{-- @include('admin.sidebar') --}}
 
-            <div class="col-md-9">
-                <div class="card">
-                    <div class="card-header">Edit Task #{{ $task->id }}</div>
+            <div class="offset-3 col-md-6">
+                <div class="card shadow">
+                    <div class="card-header">Edit Task: {{ $task->name }}</div>
                     <div class="card-body">
-                        <a href="{{ route('tasks.index') }}" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>
+                      {{--   <a href="{{ route('tasks.index') }}" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>
                         <br />
-                        <br />
+                        <br /> --}}
 
                         @if ($errors->any())
                             <ul class="alert alert-danger">
@@ -36,5 +36,5 @@
                 </div>
             </div>
         </div>
-    </div>
+    {{-- </div> --}}
 @endsection
